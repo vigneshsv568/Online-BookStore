faaf36d70dba4c6dd53c70c55dda4ac00e6ca82a
rakulrose <rakulrose@DESKTOP-5VFL5BF>
2020-05-30 13:04:34 +0530

    Initial commit....Created as Spring boot and Configure the Application Properties...

---
+++ Online_bookstore/src/test/java/in/vignesh/Online_bookstore/OnlineBookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package in.vignesh.Online_bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
